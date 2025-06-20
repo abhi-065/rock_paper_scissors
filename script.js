d3113e34128284a3c0d98328aa42a7a4110b1f80
D N S Abhiram <abhiramdns@gmail.com>
2025-06-20 11:27:26 +0000

    Wrote logic for computer choice

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    let n = Math.random();
+    if (n>=0 && n<0.3){
+        return "rock";
+    }
+    else if (n>=0.3 && n<0.5){
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}

8e1b7a8880e1a55e1a9c768fa3505cb0df495f67
D N S Abhiram <abhiramdns@gmail.com>
2025-06-20 11:57:32 +0000

    Wrote the logic for playing single round

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice===computerChoice){
+        console.log(`This is a tie! You both chose ${humanChoice}`);
+    }
+    else if (humanChoice==="rock" && computerChoice==="paper"){
+        computerScore++;
+        console.log(`You lose! Paper beats Rock`);
+    }
+    else if (humanChoice==="rock" && computerChoice==="scissors"){
+        humanScore++;
+        console.log(`You won! Rock beats Scissors`);
+    }
+    else if (humanChoice==="paper" && computerChoice==="rock"){
+        humanScore++;
+        console.log(`You won! Paper beats Rock`);
+    }
+    else if (humanChoice==="paper" && computerChoice==="scissors"){
+        computerScore++;
+        console.log(`You lose! Scissors beats Paper`);
+    }
+    else if (humanChoice==="scissors" && computerChoice==="rock"){
+        computerScore++;
+        console.log(`You lose! Rock beats Scissors`);
+    }
+    else if (humanChoice==="scissors" && computerChoice==="paper"){
+        humanScore++;
+        console.log(`You won! Scissors beats Paper`);
+    }
+    console.log(`Score of this round is: You = ${humanScore} and Computer = ${computerScore}`);
+}
+
+playRound(getHumanChoice(), getComputerChoice());

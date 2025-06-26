f2e06177cb4f7075e856230c46b14076496752e9
D N S Abhiram <abhiramdns@gmail.com>
2025-06-26 13:18:37 +0000

    Added custom events and event listeners to display live score in UI

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getComputerChoice(){
     let n = Math.random();
-    if (n>=0 && n<0.3){
+    if (n>=0 && n<0.33){
         return "rock";
     }
-    else if (n>=0.3 && n<0.5){
+    else if (n>=0.33 && n<0.66){
         return "paper";
     }
     else {
@@ -14,7 +14,29 @@ function getComputerChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
+function incrementHumanScore(){
+
+    let humanEvent = new CustomEvent('incrementHumanScore');
+
+    results.dispatchEvent(humanEvent);
+}
+
+function incrementComputerScore(){
+
+    let computerEvent = new CustomEvent('incrementComputerScore');
+
+    results.dispatchEvent(computerEvent);
+}
+
 const results = document.querySelector(".results");
+const score = document.querySelector(".score");
+
+function updateScoreDisplay(){
+    score.textContent = `Player Score: ${humanScore} and Computer Score: ${computerScore}`;
+}
+
+results.addEventListener('incrementHumanScore', updateScoreDisplay);
+results.addEventListener('incrementComputerScore', updateScoreDisplay);
 
 function playRound(humanChoice, computerChoice){
     if (humanChoice===computerChoice){
@@ -22,26 +44,32 @@ function playRound(humanChoice, computerChoice){
     }
     else if (humanChoice==="rock" && computerChoice==="paper"){
         computerScore++;
+        incrementComputerScore();
         results.textContent = `You lose! Paper beats Rock`;
     }
     else if (humanChoice==="rock" && computerChoice==="scissors"){
         humanScore++;
+        incrementHumanScore();
         results.textContent = `You won! Rock beats Scissors`;
     }
     else if (humanChoice==="paper" && computerChoice==="rock"){
         humanScore++;
+        incrementHumanScore();
         results.textContent = `You won! Paper beats Rock`;
     }
     else if (humanChoice==="paper" && computerChoice==="scissors"){
         computerScore++;
+        incrementComputerScore();
         results.textContent = `You lose! Scissors beats Paper`;
     }
     else if (humanChoice==="scissors" && computerChoice==="rock"){
         computerScore++;
+        incrementComputerScore();
         results.textContent = `You lose! Rock beats Scissors`;
     }
     else if (humanChoice==="scissors" && computerChoice==="paper"){
         humanScore++;
+        incrementHumanScore();
         results.textContent = `You won! Scissors beats Paper`;
     }
 }

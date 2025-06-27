d854412c1b0cb95ab334b091c0c774594aaf6f2f
D N S Abhiram <abhiramdns@gmail.com>
2025-06-27 14:44:45 +0000

    Added logic to play as many rounds using replay button Rearranged code to improve readability.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 function incrementHumanScore(){
+    humanScore++;
 
     let humanEvent = new CustomEvent('incrementHumanScore');
 
@@ -24,11 +25,20 @@ function incrementHumanScore(){
 }
 
 function incrementComputerScore(){
+    computerScore++;
 
     let computerEvent = new CustomEvent('incrementComputerScore');
 
     results.dispatchEvent(computerEvent);
 }
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
 
 const results = document.querySelector(".results");
 
@@ -39,9 +49,27 @@ body.appendChild(winnerSection);
 
 const announcementOfWinner = document.createElement("p");
 
+winnerSection.appendChild(announcementOfWinner);
+
+const replayBtn = document.createElement("button");
+replayBtn.textContent = "Replay";
+winnerSection.appendChild(replayBtn);
+replayBtn.addEventListener("click", function (){
+    humanScore = 0;
+    computerScore = 0;
+    announcementOfWinner.textContent = "";
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+    results.textContent = "";
+    updateScoreDisplay();
+});
+
 function updateScoreDisplay(){
     score.textContent = `Player Score: ${humanScore} and Computer Score: ${computerScore}`;
 }
+
+updateScoreDisplay();
 
 results.addEventListener('incrementHumanScore', updateScoreDisplay);
 results.addEventListener('incrementComputerScore', updateScoreDisplay);
@@ -52,58 +80,39 @@ function playRound(humanChoice, computerChoice){
         updateScoreDisplay();
     }
     else if (humanChoice==="rock" && computerChoice==="paper"){
-        computerScore++;
         incrementComputerScore();
         results.textContent = `You lose! Paper beats Rock`;
     }
     else if (humanChoice==="rock" && computerChoice==="scissors"){
-        humanScore++;
         incrementHumanScore();
         results.textContent = `You won! Rock beats Scissors`;
     }
     else if (humanChoice==="paper" && computerChoice==="rock"){
-        humanScore++;
         incrementHumanScore();
         results.textContent = `You won! Paper beats Rock`;
     }
     else if (humanChoice==="paper" && computerChoice==="scissors"){
-        computerScore++;
         incrementComputerScore();
         results.textContent = `You lose! Scissors beats Paper`;
     }
     else if (humanChoice==="scissors" && computerChoice==="rock"){
-        computerScore++;
         incrementComputerScore();
         results.textContent = `You lose! Rock beats Scissors`;
     }
     else if (humanChoice==="scissors" && computerChoice==="paper"){
-        humanScore++;
         incrementHumanScore();
         results.textContent = `You won! Scissors beats Paper`;
     }
     if (humanScore===5 || computerScore===5){
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
         if (humanScore===5){
-            announcementOfWinner.textContent = "Player Won this round!\n New round begins NOW!";
+            announcementOfWinner.textContent = "Player Won this round!\nTo play another round click replay button";
         }
         else if (computerScore===5){
-            announcementOfWinner.textContent = "Computer Won this round!\n New round begins NOW!";
+            announcementOfWinner.textContent = "Computer Won this round!\nTo play another round click replay button";
         }
-        winnerSection.appendChild(announcementOfWinner);
-        const replayBtn = document.createElement("button");
-        replayBtn.textContent = "Play another round";
-        winnerSection.appendChild(replayBtn);
-        replayBtn.addEventListener("click", function (){
-            humanScore = 0;
-            computerScore = 0;
-            announcementOfWinner.textContent = "";
-        });
     }
 }
 
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-
-rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
-paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
-scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
